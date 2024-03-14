e46ba09eb89bac54ae7e1721bfa46c5e870cc827
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2024-03-14 23:35:19 +0300

    Code cleanup

--- shaders/bloom/upsample.glsl
+++ shaders/bloom/upsample.glsl
@@ -10,25 +10,12 @@ in vec2 texCoord;
 out vec3 fragColor;
 
 void main() {
-    float x = 0;
-    float y = 0;
+    vec3 a = texture(srcTexture, vec2(texCoord.x, texCoord.y)).rgb;
 
-    vec3 a = texture(srcTexture, vec2(texCoord.x - x, texCoord.y + y)).rgb;
-    vec3 b = texture(srcTexture, vec2(texCoord.x, texCoord.y + y)).rgb;
-    vec3 c = texture(srcTexture, vec2(texCoord.x + x, texCoord.y + y)).rgb;
-
-    vec3 d = texture(srcTexture, vec2(texCoord.x - x, texCoord.y)).rgb;
-    vec3 e = texture(srcTexture, vec2(texCoord.x, texCoord.y)).rgb;
-    vec3 f = texture(srcTexture, vec2(texCoord.x + x, texCoord.y)).rgb;
-
-    vec3 g = texture(srcTexture, vec2(texCoord.x - x, texCoord.y - y)).rgb;
-    vec3 h = texture(srcTexture, vec2(texCoord.x, texCoord.y - y)).rgb;
-    vec3 i = texture(srcTexture, vec2(texCoord.x + x, texCoord.y - y)).rgb;
-
-    vec3 p = e * 4.0;
-    p += (b + d + f + h) * 2.0;
-    p += (a + c + g + i);
-    p *= 1.0 / 16.0;
+    vec3 p = a * 4.0;
+    p += (a + a + a + a) * 2.0;
+    p += (a + a + a + a);
+    p *= 1.f / 16.f;
 
     if (depth == 2) {
         p = vec3(1.0) - exp(-p * exposure);
