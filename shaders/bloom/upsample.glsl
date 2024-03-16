18dc7459f6ab4700d2f99bdc73cd91eacafdc080
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2024-03-16 11:21:27 +0300

    Code cleanup

--- shaders/bloom/upsample.glsl
+++ shaders/bloom/upsample.glsl
@@ -7,22 +7,21 @@ uniform int depth;
 uniform float exposure;
 
 in vec2 texCoord;
-out vec3 fragColor;
+out vec4 fragColor;
 
 void main() {
     vec3 a = texture(srcTexture, vec2(texCoord.x, texCoord.y)).rgb;
 
     vec3 p = a * 4.0;
-    p += (a + a + a + a) * 2.0;
-    p += (a + a + a + a);
+    p += 12.f * a;
     p *= 1.f / 16.f;
 
     if (depth == 2) {
         p = vec3(1.0) - exp(-p * exposure);
         const float gamma = 2.2;
-        fragColor = pow(p, vec3(1.0 / gamma));
+        fragColor = vec4(pow(p, vec3(1.0 / gamma)), 1.f);
     }
     else {
-        fragColor = p;
+        fragColor = vec4(p, 1.f);
     }
 }
